1a78ad6a1a83fe6cb3c29724971cd1eca9e2342f
DJ SPAER <djspaer@mail.com>
2022-09-14 12:21:43 +0300

    Second commit

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const answers = [];
 answers[0] = prompt("Name", "");
 answers[1] = prompt("Surname", "");
 answers[2] = prompt("Age", "");
+
+const comts = {
+  one: "First commit",
+  two: "Second commit"
+};
